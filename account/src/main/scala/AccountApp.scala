9ffd822756152fcd8acc899b95032b35644ff04b
Efimka <efimzik5@gmail.com>
2023-06-07 16:12:13 +0300

    Launching the "Account" application. Sending a command to create and replenish an account

--- account/src/main/scala/AccountApp.scala
+++ account/src/main/scala/AccountApp.scala
@@ -1,6 +1,9 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import com.typesafe.config.ConfigFactory
+import kafka.Streams
+import repository.Repository
+import model.AccountUpdate
 import route.Route
 
 
@@ -8,8 +11,16 @@ object AccountApp extends App {
   implicit val system: ActorSystem = ActorSystem("App")
   implicit val ec = system.dispatcher
 
-  private val route = new Route()
   val port = ConfigFactory.load().getInt("port")
+  val accountId = ConfigFactory.load().getInt("account.id")
+  val defAmount = ConfigFactory.load().getInt("account.amount")
+
+  private val repository = new Repository(accountId, defAmount)
+  private val streams = new Streams(repository)
+
+  streams.produceCommand(AccountUpdate(100))
+
+  private val route = new Route()
 
   Http().newServerAt("0.0.0.0", port).bind(route.routes)
 }
