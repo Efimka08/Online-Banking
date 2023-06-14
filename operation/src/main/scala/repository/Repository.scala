22c448eb7950feec7c7f6908e1e98120f4e3483b
Efimka <efimzik5@gmail.com>
2023-06-14 22:23:29 +0300

    Correct transfer v3.1 (+comments removed)

--- operation/src/main/scala/repository/Repository.scala
+++ operation/src/main/scala/repository/Repository.scala
@@ -7,11 +7,9 @@ import io.circe.generic.auto._
 import scala.concurrent.Future
 
 class Repository(streams: OperationStreams){
-    //implicit val commandTopicName: TopicName[AccountUpdate] = streams.simpleTopicName[AccountUpdate]
 
     def transfer(transfer: TransferStart) = {
         if (transfer.value > 0) {
-            //streams.checkAccountUpdateEvent(transfer.transferId, transfer.destinationId)
             implicit val commandTopicName: TopicName[AccountUpdate] = streams.simpleTopicName[AccountUpdate]
             streams.produceCommand(AccountUpdate(transfer.transferId, transfer.sourceId, Some(transfer.destinationId), -transfer.value, transfer.category))
             println(s"При переводе №${transfer.transferId} со счета ${transfer.sourceId} была снята сумма ${-transfer.value}.")
